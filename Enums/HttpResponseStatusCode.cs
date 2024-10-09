be413be70bf229078284199370e576412caac2d3
Student <Student@PC-141-11>
2024-10-08 23:01:27 -0700

    Add project files.

---
+++ Enums/HttpResponseStatusCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniServer.HTTP.Enums
+{
+    public enum HttpResponseStatusCode
+    {
+        OK = 200,
+        Created = 201,
+        Found = 302,
+        SeeOther = 303,
+        BadReaquest = 400,
+        Unauthorized = 403,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniServer.HTTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
